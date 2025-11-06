0cf9c5db3dc717bbeb3480466ec0394cf3651de7
Le Minh Hieu <106205555@student.swin.edu.au>
2025-11-06 09:58:43 +0700

    Update 20b: Editing process_eoi.php

--- process_eoi.php
+++ process_eoi.php
@@ -1,104 +1,94 @@
 <?php
 require_once("settings.php");
 
-// Checks if there is a table named 'eoi'. If not, creates one.
-$eoiquery1 = "SHOW TABLES LIKE 'eoi'";
-$eoicheck = mysqli_query($conn, $eoiquery1);
-if(mysqli_num_rows($eoicheck) == 0) {
-    $eoiquery2 = "CREATE TABLE eoi (
-        eoi_id INT AUTO_INCREMENT PRIMARY KEY,
-        job_id VARCHAR(5),
-        first_name VARCHAR(20),
-        last_name VARCHAR(20),
-        street VARCHAR(40),
-        town VARCHAR(40),
-        state VARCHAR(3),
-        postcode INT,
-        email VARCHAR(50),
-        phone VARCHAR(12),
-        programming_lang VARCHAR(6),
-        otherskills VARCHAR(100),
-        status VARCHAR(7)
-        )";
-    $eoicreate = mysqli_query($conn, $eoiquery2);
-}
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Checks if there is a table named 'eoi'. If not, creates one.
+    $eoiquery1 = "SHOW TABLES LIKE 'eoi'";
+    $eoicheck = mysqli_query($conn, $eoiquery1);
+    if(mysqli_num_rows($eoicheck) == 0) {
+        $eoiquery2 = "CREATE TABLE eoi (
+            eoi_id INT AUTO_INCREMENT PRIMARY KEY,
+            job_id VARCHAR(5),
+            first_name VARCHAR(20),
+            last_name VARCHAR(20),
+            street VARCHAR(40),
+            town VARCHAR(40),
+            state VARCHAR(3),
+            postcode INT,
+            email VARCHAR(50),
+            phone VARCHAR(12),
+            programming_lang VARCHAR(6),
+            otherskills VARCHAR(100),
+            status VARCHAR(7)
+            )";
+        $eoicreate = mysqli_query($conn, $eoiquery2);
+    }
 
-// Extracts and validates data received from form
-if(empty($_POST['firstname'])) {
-    $missings[] = "First name";
-} else {
-    $firstname = mysqli_real_escape_string($conn, $_POST['firstname']);
-}
+    // Extracts and validates data received from form
+    $firstname = clean_input($conn, $_POST["firstname"]);
+    $lastname = clean_input($conn, $_POST["lastname"]);
+    $street = clean_input($conn, $_POST["street"]);
+    $town = clean_input($conn, $_POST["town"]);
+    $state = clean_input($conn, $_POST["state"]);
+    $postcode = clean_input($conn, $_POST["postcode"]);
+    $email = clean_input($conn, $_POST["email"]);
+    $phone = clean_input($conn, $_POST["phone"]);
+    $jobrefnum = clean_input($conn, $_POST["jobrefnum"]);
+    $prglang = clean_input($conn, $_POST["prglang"]);
+    $otherskills = clean_input($conn, $_POST["otherskills"]);
 
-if(empty($_POST['lastname'])) {
-    $missings[] = "Last name";
-} else {
-    $lastname = mysqli_real_escape_string($conn, $_POST['lastname']);
-}
+    if(empty($firstname)) {
+        $missings[] = "First name";
+    }
 
-if(empty($_POST['street'])) {
-    $missings[] = "Street address";
-} else {
-    $street = mysqli_real_escape_string($conn, $_POST['street']);
-}
+    if(empty($lastname)) {
+        $missings[] = "Last name";
+    }
 
-if(empty($_POST['town'])) {
-    $missings[] = "Suburb/town";
-} else {
-    $town = mysqli_real_escape_string($conn, $_POST['town']);
-}
+    if(empty($street)) {
+        $missings[] = "Street address";
+    }
 
-if(empty($_POST['state'])) {
-    $missings[] = "State";
-} else {
-    $state = mysqli_real_escape_string($conn, $_POST['state']);
-}
+    if(empty($town)) {
+        $missings[] = "Suburb/town";
+    }
 
-if(empty($_POST['postcode'])) {
-    $missings[] = "Postcode";
-} else {
-    $postcode = mysqli_real_escape_string($conn, $_POST['postcode']);
-}
+    if(empty($state)) {
+        $missings[] = "State";
+    }
 
-if(empty($_POST['email'])) {
-    $missings[] = "Email address";
-} else {
-    $email = mysqli_real_escape_string($conn, $_POST['email']);
-    if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    if(empty($postcode)) {
+        $missings[] = "Postcode";
+    }
+
+    if(empty($email)) {
+        $missings[] = "Email address";
+    } elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
         $errors[] = "Invalid email address.";
     }
-}
 
-if(empty($_POST['phone'])) {
-    $missings[] = "Phone number";
-} else {
-    $phone = mysqli_real_escape_string($conn, $_POST['phone']);
-}
-
-if(empty($_POST['jobrefnum'])) {
-    $missings[] = "Job reference number";
-} else {
-    $jobrefnum = mysqli_real_escape_string($conn, $_POST['jobrefnum']);
-}
-
-if(empty($_POST['prglang'])) {
-    $missings[] = "Programming languages";
-} else {
-    $prglang = mysqli_real_escape_string($conn, $_POST['prglang']);
-}
-
-// Checks for errors and inserts data into table
-if(empty($missings) && empty($errors)) {
-    echo "<p>Thank you!</p>";
-} elseif(isset($missing)) {
-    echo "<p>Please fill in all the required fields.</p> <p>Missing fields:</p>";
-    foreach($missings as $missing) {
-        echo "<p>" .$missing. "</p>";
+    if(empty($phone)) {
+        $missings[] = "Phone number";
     }
-} else {
-    echo "<p><strong>Error:</strong></p>";
-    foreach($errors as $error) {
-        echo "<p>" .$error. "</p>";
+
+    if(empty($jobrefnum)) {
+        $missings[] = "Job reference number";
     }
+
+    if(empty($prglang)) {
+        $missings[] = "Programming languages";
+    }
+
+    // Checks for errors and inserts data into table
+    
+}
+
+// Checks integrity of data
+function clean_input($conn, $data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    $data = mysqli_real_escape_string($conn, $data);
+    return $data;
 }
 ?>
